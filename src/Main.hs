f07b0bae4036b2962943f850af95b7e66e729f4c
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-10-05 14:06:59 +0900

    tried with System.IO.Temp.withSystemTempFile, but failed

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,23 @@
 module Main where
 
 import Web.Scotty
+import Control.Monad.IO.Class
+import qualified Data.ByteString.Lazy as BSL
+import System.IO
+import System.Process
+import System.IO.Temp
+import System.Environment
+import System.Directory
 
 main :: IO ()
-main = scotty 61000 $ get "/" $ html "<h1> hello, world!"
+main = scotty 61000 $ do
+  post "/" $ do
+    bs <- body
+    name <- liftIO getProgName
+    response <- liftIO $ withSystemTempFile name $ \path h -> do
+      BSL.putStrLn bs
+      BSL.hPut h bs
+      hClose h
+      callProcess "gvim" [path]
+      BSL.readFile path
+    raw response
