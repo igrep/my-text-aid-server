daeddb2b103f743dd68662ccc17d94b55141ff17
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-10-05 18:20:14 +0900

    try to delete temporary files after editing

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ module Main where
 
 import Web.Scotty
 import Control.Monad.IO.Class
+import Control.Monad
 import qualified Data.ByteString.Lazy as BSL
 import System.IO
 import System.Process
@@ -14,12 +15,25 @@ main :: IO ()
 main = scotty 61000 $ do
   post "/" $ do
     bs <- body
-    response <- liftIO $ do
-      name <- getProgName
-      tempDir <- getTemporaryDirectory
-      (path, h) <- openBinaryTempFile tempDir name
-      BSL.hPut h bs
-      hClose h
-      callProcess "gvim" [path]
-      BSL.readFile path
-    raw response
+    (path, h) <- liftIO createTemporaryFileForEditor
+    raw =<< (liftIO $ editBinaryOnFile bs path h)
+
+removeFileIfExist :: FilePath -> IO ()
+removeFileIfExist path = do
+  b <- doesFileExist path
+  when b $ removeFile path
+
+createTemporaryFileForEditor :: IO (FilePath, Handle)
+createTemporaryFileForEditor = do
+  name <- getProgName
+  tempDir <- getTemporaryDirectory
+  openBinaryTempFile tempDir name
+
+editBinaryOnFile :: BSL.ByteString -> FilePath -> Handle -> IO BSL.ByteString
+editBinaryOnFile bs path h = do
+  BSL.hPut h bs
+  hClose h
+  callProcess "gvim" [path]
+  r <- BSL.readFile path
+  removeFileIfExist path
+  return r
